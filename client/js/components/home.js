193efb7d21ed11aac346eaf189dcb7cf24e867f7
xiomara-masmela <xiomara-masmela@gmail.com>
2021-10-23 16:06:59 +1000

    First commit

---
+++ client/js/components/home.js
@@ -0,0 +1,36 @@
+function home() {
+    const page = document.getElementById('page');
+    //Search Form 
+    page.classList.add("container");
+    
+    const searchForm = document.createElement('form');
+    const searchInput = document.createElement('input');
+    searchInput.setAttribute("type", "text");
+    const searchButton = document.createElement('input')
+    searchButton.setAttribute("type", "submit");
+
+    searchForm.append(searchInput);
+    searchForm.append(searchButton);
+    
+    page.append(searchForm);
+
+    //Products div
+    const productsContainer = document.createElement('div');
+    productsContainer.classList.add("col");
+
+    //Single Product
+    const productSingle = document.createElement('div');
+
+    const productTitle = document.createElement('h4');
+    productTitle.innerHTML = "Product Title"
+    const productPrice = document.createElement('p');
+    productPrice.innerHTML = "35";
+
+    productSingle.append(productTitle);
+    productSingle.append(productPrice);
+
+    productsContainer.append(productSingle);
+
+    page.append(productsContainer);
+
+}
