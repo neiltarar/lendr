ef5a76c0a1bd8a3c01af6ffa2845e7f02a870ba6
Anil Tarar <aniltarar@gmail.com>
2021-10-23 17:17:48 +1100

    Heroku deployment

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static("client"));
 
-
+app.get('/', (req, res) => {
+    res.send('hello')
+  });
 
 
 app.listen(port, () => {
