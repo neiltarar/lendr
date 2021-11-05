b0c49277a75c780764c27aabf78264387cc08b68
Lucy Kendall <lucykendall88@gmail.com>
2021-11-05 17:33:05 +1100

    api with search

--- client/js/components/autocomplete.js
+++ client/js/components/autocomplete.js
@@ -1,15 +1,15 @@
-function initAutocomplete() {
-  const address = document.getElementById('address');
-  console.log(address)
-  const autocomplete = new google.maps.places.Autocomplete(address);
+function initAutocomplete(addressFieldId) {
+    const address = document.getElementById(addressFieldId);
+    const autocomplete = new google.maps.places.Autocomplete(address);
 
-  autocomplete.addListener('place_changed', function() {
-    const place = autocomplete.getPlace();
-    console.log(place)
-    const latitude = place.geometry.location.lat();
-    console.log(latitude)
-    const longitude = place.geometry.location.lng();
-    document.getElementById('lat').value = latitude;
-    document.getElementById('lng').value = longitude;
-  });
-};
+    autocomplete.addListener('place_changed', function() {
+      const place = autocomplete.getPlace();
+      const latitude = place.geometry.location.lat();
+      console.log(latitude)
+      const longitude = place.geometry.location.lng();
+      console.log(longitude)
+      document.getElementById('lat').value = latitude;
+      document.getElementById('lng').value = longitude;
+      document.getElementById('formattedaddress').value = place["formatted_address"];
+    });
+  };
