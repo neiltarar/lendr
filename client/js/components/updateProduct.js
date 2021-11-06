89eba11ee4de8f40c735223cd63ad0e2a3071b95
Lucy Kendall <lucykendall88@gmail.com>
2021-11-06 14:06:29 +1100

    fix heroku

--- client/js/components/updateProduct.js
+++ client/js/components/updateProduct.js
@@ -1,45 +1,57 @@
 const renderUpdateProduct = (id) => {
-    const page = document.getElementById("page");
-    page.innerHTML = ''
-  
-    const form = document.createElement("form");
-    form.className = 'Form'
-    form.innerHTML = `
-        <fieldset>
-          <label for="name">Name:</label><br>
-          <input type="text" name="name">
-        </fieldset>
-        <fieldset>
-          <label for="description">Description:</label><br>
-          <input type="text" name="description">
-        </fieldset>
-        <fieldset>
-          <label for="address">Address:</label><br>
-          <input type="text" name="address">
-        </fieldset>
-        <fieldset>
-          <label for="availability">Availability: </label><br>
-          <input type="date" name="availability">
-        </fieldset>
-        <fieldset>
-          <label for="image">Image: </label><br>
-          <input type="text" name="image">
-        </fieldset>
-        <fieldset>
-          <label for="category">Choose a category: </label><br>
-            <select name="category" id="category">
-              <option value="Appliance">Appliance</option>
-              <option value="Outdoor">Outdoor</option>
-              <option value="Exercise">Exericse</option>
-              <option value="Kitchen">Kitchen</option>
-            </select>
-        </fieldset>
-        <fieldset>
-          <label for="Price">Price: </label><br>
-          <input type="number" name="price">
-        </fieldset>
-        <input type="submit" class="button">
-        `;
+  const page = document.getElementById("page");
+  page.innerHTML = ''
+  const formRow = document.createElement("div");
+  formRow.classList.add("row");
+  formRow.classList.add("mt-5");
+  formRow.classList.add("pt-5");
+  formRow.classList.add("justify-content-center");
+  formRow.classList.add("align-items-center");
+  const formCol = document.createElement("div");
+  formCol.classList.add("col-6");
+  const form = document.createElement("form");
+  form.className = 'Form'
+  form.innerHTML = `
+      <fieldset>
+        <label class="form-label"for="name">Name:</label><br>
+        <input class="w-100 form-control" type="text" name="name">
+      </fieldset>
+      <fieldset>
+        <label class="form-label" for="description">Description:</label><br>
+        <input class="w-100 form-control" type="text" name="description">
+      </fieldset>
+      <fieldset>
+        <label class="form-label" for="availability">Availability: </label><br>
+        <input class="w-100 form-control" type="date" name="availability">
+      </fieldset>
+      <fieldset>
+      <label class="form-label" for="address">Address:</label><br>
+      <input class="w-100 form-control" type="text" id="address" name="address"></input>
+          <input type="hidden" id="lat" name="latitude" value=""></input>
+          <input type="hidden" id="lng" name="longitude" value=""></input>
+          <input type="hidden" id="formattedaddress" name="formattedaddress" value=""></input>
+      </fieldset>
+      <fieldset>
+        <label class="form-label" for="image">Image: </label><br>
+        <input class="w-100 form-control" type="text" name="image">
+      </fieldset>
+      <fieldset>
+        <label class="form-label" for="category">Choose a category: </label><br>
+          <select class="form-select" name="category" id="category">
+            <option value="Appliance">Appliance</option>
+            <option value="Outdoor">Outdoor</option>
+            <option value="Exercise">Exercise</option>
+            <option value="Kitchen">Kitchen</option>
+          </select>
+      </fieldset>
+      <fieldset>
+        <label class="form-label"for="Price">Price: </label><br>
+        <input class="w-100 form-control"type="number" name="price">
+      </fieldset>
+      
+      <input type="submit" class="btn btn-blue w-100">
+      `;
+
   
     form.addEventListener("submit", (event) => {
       event.preventDefault()
@@ -60,5 +72,8 @@ const renderUpdateProduct = (id) => {
       })
       renderHome()
     })
-    page.replaceChildren(form);
+    formCol.appendChild(form);
+    formRow.appendChild(formCol);
+    page.appendChild(formRow);
+    initAutocomplete('address');
   };
