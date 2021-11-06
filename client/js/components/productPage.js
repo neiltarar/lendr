6c7dd8ca979e29bd35fae8d7d545b2cfe72e2864
Lucy Kendall <lucykendall88@gmail.com>
2021-11-06 14:02:58 +1100

    update page

--- client/js/components/productPage.js
+++ client/js/components/productPage.js
@@ -43,21 +43,21 @@ const productPage = (id) => {
     productBox.append(buttonContainer);
 
     // Update Product Button
-    const updateProduct = document.createElement("button"); //Add button to link to add product page
-    updateProduct.setAttribute("type", "button");
-    updateProduct.textContent = "Update Product";
-    updateProduct.classList.add("btn");
-    updateProduct.classList.add("btn-blue");
-    // updateProduct.innerHTML = `<button type="button" class="btn btn-blue">Update Product</button`
-    buttonContainer.append(updateProduct); //may need to append to different html element
-    updateProduct.addEventListener("click", (event) => {
-      id = product["id"];
-      console.log(id);
-      axios.get(`/api/products/${id}`).then((response) => {
-        console.log(response);
-        renderUpdateProduct(id);
-      });
-    });
+    // const updateProduct = document.createElement("button"); //Add button to link to add product page
+    // updateProduct.setAttribute("type", "button");
+    // updateProduct.textContent = "Update Product";
+    // updateProduct.classList.add("btn");
+    // updateProduct.classList.add("btn-blue");
+    // // updateProduct.innerHTML = `<button type="button" class="btn btn-blue">Update Product</button`
+    // buttonContainer.append(updateProduct); //may need to append to different html element
+    // updateProduct.addEventListener("click", (event) => {
+    //   id = product["id"];
+    //   console.log(id);
+    //   axios.get(`/api/products/${id}`).then((response) => {
+    //     console.log(response);
+    //     renderUpdateProduct(id);
+    //   });
+    // });
     // Delete Product Button
     const deleteProduct = document.createElement("button"); //delete product
     deleteProduct.classList.add("btn", "btn-outline-blue");
