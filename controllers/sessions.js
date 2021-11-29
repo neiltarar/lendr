6e998cc27551a9c480eedc8235bfeda6da2664c0
Lucy Kendall <lucykendall88@gmail.com>
2021-11-29 17:01:25 +1100

    test new com

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -3,16 +3,16 @@ const session = express.Router();
 
 session.get("/", (req, res) => {
     if (req.session.username) {
-        res.status(200).json({message: `Welcome back, ${req.session.username}`, username: req.session.username, userId: req.session.userId});
-        
+        res.status(200).json({ message: `Welcome back, ${req.session.username}`, username: req.session.username, userId: req.session.userId });
+
     } else {
-        res.status(401).json({message: "You are not logged in"});
+        res.status(401).json({ message: "You are not logged in" });
     }
 })
 
-session.delete("/" , (req, res) => {  
+session.delete("/", (req, res) => {
     req.session.destroy();
-    res.json({message: "You have successfully logged out"});
+    res.json({ message: "You have successfully logged out" });
 });
 
 module.exports = session;
