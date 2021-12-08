a9ef072ee907ed36e83db8eebc607a9fff42c846
Neil Tarar <aniltarar@gmail.com>
2021-12-08 14:11:33 +1100

    Cloudinary image upload done, rendering the page is work in progress

--- client/js/components/form.js
+++ client/js/components/form.js
@@ -1,17 +1,23 @@
-const renderNewProduct = () => {
-  const page = document.getElementById("page");
-  page.innerHTML = "";
-  const formRow = document.createElement("div");
-  formRow.classList.add("row");
-  formRow.classList.add("mt-5");
-  formRow.classList.add("pt-5");
-  formRow.classList.add("justify-content-center");
-  formRow.classList.add("align-items-center");
-  const formCol = document.createElement("div");
-  formCol.classList.add("col-6");
-  const form = document.createElement("div");
+function handleSubmit(event) {
+  const myForm = document.getElementById("myForm");
+  myForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
+    axios.post("/", "product image").then((res) => {
+      const page = document.getElementById("page");
+      page.innerHTML = "";
+      const formRow = document.createElement("div");
+      formRow.classList.add("row");
+      formRow.classList.add("mt-5");
+      formRow.classList.add("pt-5");
+      formRow.classList.add("justify-content-center");
+      formRow.classList.add("align-items-center");
+      const formCol = document.createElement("div");
+      formCol.classList.add("col-6");
+      const form = document.createElement("div");
 
-  form.innerHTML = `
+      form.innerHTML = `
       <form method="POST" action="/api/users/products/host" enctype="multipart/form-data">
         <div>
           <label class="form-label"for="name">Name:</label><br>
@@ -52,8 +58,12 @@ const renderNewProduct = () => {
       </form>
       `;
 
-  formCol.appendChild(form);
-  formRow.appendChild(formCol);
-  page.appendChild(formRow);
-  initAutocomplete("address");
-};
+      formCol.appendChild(form);
+      formRow.appendChild(formCol);
+      page.appendChild(formRow);
+      initAutocomplete("address");
+    });
+  });
+}
+
+exports.handleSubmit;
--- controllers/usersProducts.js
+++ controllers/usersProducts.js
@@ -6,95 +6,131 @@ const connectPgSimple = require("connect-pg-simple");
 const usersProductsController = express.Router();
 // Get date and time to use in reviews
 const today = new Date();
-const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+const date =
+  today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+const time =
+  today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 //Node geo-coder//
 const dotenv = require("dotenv");
-const key = process.env.GEOCODING_API_KEY
-const NodeGeocoder = require('node-geocoder');
+const key = process.env.GEOCODING_API_KEY;
+const NodeGeocoder = require("node-geocoder");
 const { response } = require("express");
 const options = {
-    provider: 'google',
+  provider: "google",
 
-    // Optional depending on the providers
-    apiKey: key, // for Mapquest, OpenCage, Google Premier
-    formatter: null // 'gpx', 'string', ...
+  // Optional depending on the providers
+  apiKey: key, // for Mapquest, OpenCage, Google Premier
+  formatter: null, // 'gpx', 'string', ...
 };
 const geocoder = NodeGeocoder(options);
 
+//Cloudinary
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const multer = require("multer");
+// // cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "LENDR-PRODUCT-IMAGES",
+  },
+});
+
+const upload = multer({ storage: storage });
+
+// REVIEW SECTION
 usersProductsController.post("/review", sessionAuth, (req, res, next) => {
-    dateTime = date + " " + time;
-    let { rating, productId, review } = req.body; 
-    console.log(req.body);
-    // If no rating is chosen it assigns it to 0 to avoid rendering NAN on the webpage.
-    if (rating === undefined) {
-        rating = 0;
-    }
-    const userEmail = req.session.username;
-    usersDB.getUser(userEmail).then((res) => {
-        const userId = res[0].user_id;
-        usersProductsDB.addReview(review, rating, dateTime, userId, productId);
-    });
+  dateTime = date + " " + time;
+  let { rating, productId, review } = req.body;
+  console.log(req.body);
+  // If no rating is chosen it assigns it to 0 to avoid rendering NAN on the webpage.
+  if (rating === undefined) {
+    rating = 0;
+  }
+  const userEmail = req.session.username;
+  usersDB.getUser(userEmail).then((res) => {
+    const userId = res[0].user_id;
+    usersProductsDB.addReview(review, rating, dateTime, userId, productId);
+  });
 
-    res.json({ status: "review added" });
+  res.json({ status: "review added" });
 });
 
+// DELETE PRODUCT
 usersProductsController.delete("/:id", sessionAuth, (req, res) => {
-    //delete product by id
-    const id = req.params.id;
-    const userId = req.session.userId;
-    console.log(userId);
+  //delete product by id
+  const id = req.params.id;
+  const userId = req.session.userId;
+  console.log(userId);
 
-    usersProductsDB.deleteProduct(id, userId).then(() => {
-        res.status(200).send();
-        console.log(`deleted product: ${id}`);
-    });
+  usersProductsDB.deleteProduct(id, userId).then(() => {
+    res.status(200).send();
+    console.log(`deleted product: ${id}`);
+  });
 });
 
-usersProductsController.post("/host", sessionAuth, (req, res) => { //add product
-    const { name, description, availability, formattedaddress, longitude, latitude, image, category, price } = req.body
-    console.log(req.body)
+// ADD A NEW PRODUCT (HOST A NEW ITEM)
+usersProductsController.post(
+  "/host",
+  upload.single("product-image"),
+  function (req, res, next) {
+    // req.file is the `profile-file` file
+    console.log(JSON.stringify(req.file.path));
+    res.json({ status: "image uploaded" });
+    next();
+  }
+);
 
-    const userId = req.session.userId //getting user_id from sessions
-    console.log(userId)
+// usersProductsController.post("/host", sessionAuth, (req, res) => { //add product
+//     const { name, description, availability, formattedaddress, longitude, latitude, image, category, price } = req.body
+//     console.log(req.body)
 
-    if (name === undefined || name === '' || name.length > 20) {
-        res.status(400).json({ message: 'name not defined' })
-        return
-    } else if (description === undefined || description === '' || description.length > 50) {
-        res.status(400).json({ message: 'description not defined' })
-        return
-    } else if (formattedaddress === undefined || formattedaddress === '') {
-        console.log(formattedaddress)
-        console.log("formatted address not found")
-        return
-    } else if (longitude === undefined || longitude === '') {
-        console.log("longitude not found")
-        return
-    } else if (latitude === undefined || latitude === '') {
-        console.log("latitude not found")
-        return
-    } else if (availability === undefined || availability === '') {
-        res.status(400).json({ message: 'availability not defined' })
-        return
-    } else if (image === undefined || image === '') {
-        res.status(400).json({ message: 'imageurl not defined' })
-        return
-    } else if (category === undefined || category === '') {
-        res.status(400).json({ message: 'category not defined' })
-    } else if (price === undefined || price === '') {
-        res.status(400).json({ message: 'price not defined' })
-    } else {
-        console.log("adding product")
-        // geocode(address).then((geocodedaddress) => {
-        //     console.log(geocodedaddress)
+//     const userId = req.session.userId //getting user_id from sessions
+//     console.log(userId)
 
-        usersProductsDB.addNewProduct(name, description, formattedaddress, longitude, latitude, availability, image, category, price, userId).then((products) => {
-            res.status(201).send(products)
-            console.log(`added product: ${name}`);
-        })
-    };
-});
+//     if (name === undefined || name === '' || name.length > 20) {
+//         res.status(400).json({ message: 'name not defined' })
+//         return
+//     } else if (description === undefined || description === '' || description.length > 50) {
+//         res.status(400).json({ message: 'description not defined' })
+//         return
+//     } else if (formattedaddress === undefined || formattedaddress === '') {
+//         console.log(formattedaddress)
+//         console.log("formatted address not found")
+//         return
+//     } else if (longitude === undefined || longitude === '') {
+//         console.log("longitude not found")
+//         return
+//     } else if (latitude === undefined || latitude === '') {
+//         console.log("latitude not found")
+//         return
+//     } else if (availability === undefined || availability === '') {
+//         res.status(400).json({ message: 'availability not defined' })
+//         return
+//     } else if (image === undefined || image === '') {
+//         res.status(400).json({ message: 'imageurl not defined' })
+//         return
+//     } else if (category === undefined || category === '') {
+//         res.status(400).json({ message: 'category not defined' })
+//     } else if (price === undefined || price === '') {
+//         res.status(400).json({ message: 'price not defined' })
+//     } else {
+//         console.log("adding product")
+//         // geocode(address).then((geocodedaddress) => {
+//         //     console.log(geocodedaddress)
+
+//         usersProductsDB.addNewProduct(name, description, formattedaddress, longitude, latitude, availability, image, category, price, userId).then((products) => {
+//             res.status(201).send(products)
+//             console.log(`added product: ${name}`);
+//         })
+//     };
+// });
 // geocode(address).then((geocodedaddress) => {
 //     console.log(geocodedaddress)
 
@@ -128,9 +164,9 @@ usersProductsController.post("/host", sessionAuth, (req, res) => { //add product
 //     } else if (image === undefined || image === '') {
 //         res.status(400).json({ message: 'imageurl not defined' })
 //         return
-//     } else if (category === undefined || category === '') { 
+//     } else if (category === undefined || category === '') {
 //         res.status(400).json({ message: 'category not defined' })
-//     } else if (price === undefined || price === '' ) { 
+//     } else if (price === undefined || price === '' ) {
 //         res.status(400).json({ message: 'price not defined' })
 //     } else {
 //         usersProductsDB.addNewProduct(name, description, address, availability, image, category, price, productId, userId).then((products) => {
